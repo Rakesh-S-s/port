a21f485b1c4a5c0b5d7e5a340c3e76067160b32a
rakesh <rakraj1602@gmail.com>
2023-12-10 21:08:44 +0530

    add skill project internship

--- src/app.js
+++ src/app.js
@@ -1,8 +1,35 @@
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
 
+const blog = document.getElementById('blog')
+const online = document.getElementById('online')
+const attendance = document.getElementById('attendance')
+const investment = document.getElementById('investment')
+const weather = document.getElementById('weather')
+
+blog.addEventListener('click', () => {
+    window.open('https://rakesh-blogit.netlify.app', '_blank')
+})
+
+online.addEventListener('click', () => {
+    window.open('https://github.com/Rakesh-S-s/Online_Infrigement_detection', '_blank')
+})
+
+attendance.addEventListener('click', () => {
+    window.open('https://github.com/Rakesh-S-s/Automatic_attendance_system', '_blank')
+})
+
+investment.addEventListener('click', () => {
+    window.open('https://github.com/Rakesh-S-s/PitchTank', '_blank')
+})
+
+weather.addEventListener('click', () => {
+    window.open('https://weatherapp-black-beta.vercel.app', '_blank')
+})
+
 function toggleButton(){
     navList.classList.toggle('show')
 }
 
 hamburgerButton.addEventListener('click',toggleButton)
+
